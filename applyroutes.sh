79802d461eb0a413c1de46c20723285f1a3e7998
Rik Bignell <bignellrp@gmail.com>
2019-02-08 16:02:02 +0000

    Typo on Test1 in applyroutes. Plus added 8.8.8.8 to testing

--- applyroutes.sh
+++ applyroutes.sh
@@ -8,7 +8,7 @@ routetableid=`aws cloudformation describe-stacks --stack-name fortigate-egress -
 eniid=`aws cloudformation describe-stacks --stack-name fortigate-egress --output text | awk '/InsideENI/ {print $3}'`
 tgwid=`aws cloudformation describe-stacks --stack-name fortigate-egress --output text | awk '/TransitGatewayOutput/ {print $4}'`
 spoke1rtbid=`aws cloudformation describe-stacks --stack-name fortigate-spokes --output text | awk '/Test1RouteTableID/ {print $3}'`
-spoke2rtbid=`aws cloudformation describe-stacks --stack-name fortigate-spokes --output text | awk '/Test1RouteTableID/ {print $3}'`
+spoke2rtbid=`aws cloudformation describe-stacks --stack-name fortigate-spokes --output text | awk '/Test2RouteTableID/ {print $3}'`
 vpc1instanceprivate=`aws cloudformation describe-stacks --stack-name fortigate-spokes --output text | awk '/Test1InstancesPrivateIp/ {print $3}'`
 vpc1instancepublic=`aws cloudformation describe-stacks --stack-name fortigate-spokes --output text | awk '/Test1InstancesPublicIp/ {print $3}'`
 vpc2instanceprivate=`aws cloudformation describe-stacks --stack-name fortigate-spokes --output text | awk '/Test2InstancesPrivateIp/ {print $3}'`
@@ -31,9 +31,12 @@ aws ec2 create-route --route-table-id $spoke2rtbid --destination-cidr-block 0.0.
 
 echo "
 
-Now login to test instance in VPC 1 and confirm you can ping the instance in VPC2
+Now login to test instance in VPC 1 and confirm you can ping the instance in VPC2. 
+Also test that both VPC1 and VPC2 can get to 8.8.8.8 through the FW.
 
 ssh -oStrictHostKeyChecking=no -i ~/.ssh/$key.pem ec2-user@$vpc1instancepublic ping $vpc2instanceprivate
+ssh -oStrictHostKeyChecking=no -i ~/.ssh/$key.pem ec2-user@$vpc1instancepublic ping 8.8.8.8
+ssh -oStrictHostKeyChecking=no -i ~/.ssh/$key.pem ec2-user@$vpc2instancepublic ping 8.8.8.8
 
 
 "
